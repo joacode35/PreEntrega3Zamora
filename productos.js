134080357a5b0d22855e4a4047c9474702b5195f
joaquincode35 <joaquincode35@gmail.com>
2023-09-21 15:25:43 -0300

    formulario y carrito

--- productos.js
+++ productos.js
@@ -1,105 +1,48 @@
-//Estructura de datos
-class Stock_fitness {
-  constructor(producto, precio, stock) {
-    this.producto = producto;
-    this.precio = precio;
-    this.stock = stock;
+//DOM Y EVENTOS
+
+document.addEventListener("DOMContentLoaded", function () {
+  let Formulario = document.getElementById("Formulario");
+  let inicio = document.getElementById("inicio");
+
+  let storedUser = localStorage.getItem("user");
+
+  if (storedUser) {
+    inicio.textContent = `¡Hola, ${storedUser}! ya ingresaste!.`;
+    Formulario.style.display = "none";
+  } else {
+    Formulario.addEventListener("submit", function (e) {
+      e.preventDefault();
+      let username = document.getElementById("username").value;
+      let password = document.getElementById("password").value;
+
+      //LOCALSTORAGE
+      if (username === "joaquin" && password === "zamora") {
+        localStorage.setItem("user", username);
+
+        inicio.textContent = `¡bienvenido a la tienda, ${username}! :)`;
+        Formulario.style.display = "none";
+      } else {
+        inicio.textContent = "Contraseña incorrecta, intentalo de nuevo.";
+      }
+    });
   }
+});
 
-  get_datos() {
-    console.log("----productos----");
-    console.log("producto:", this.producto);
-    console.log("precio:", this.precio);
-    console.log("stock:", this.stock);
-  }
+//DOM Y EVENTOS CARRITO
+let carrito = document.getElementsByClassName("agregarCarrito");
+console.log(carrito);
 
-  get_stock() {
-    if (this.stock > 0) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  renovar_stock(cantidades) {
-    if (this.stock >= cantidades) {
-      this.stock = this.stock - cantidades;
-      return true;
-    } else {
-      console.log("No tenemos stock suficiente");
-    }
-  }
-}
-
-//Productos
-let listaProductos = [];
-
-listaProductos.unshift(new Stock_fitness("proteina", 15000, 4));
-listaProductos.unshift(new Stock_fitness("pressbanca", 40000, 2));
-listaProductos.unshift(new Stock_fitness("gymball", 10000, 0));
-listaProductos.unshift(new Stock_fitness("corebag", 8000, 10));
-listaProductos.unshift(new Stock_fitness("creatina", 20000, 3));
-
-//Filtrado de stock
-for (let Stock_fitness of listaProductos) {
-  if (Stock_fitness.stock > 0) {
-    Stock_fitness.get_datos();
-  }
-}
-
-//Compra
-let pregunta_lista = prompt(
-  "¿quiere ver la lista de precios? responda con si o no"
-);
-
-let compraProducto;
-
-let compraProductoPrompt = () => {
-  compraProducto = prompt(
-    "Ingrese el nombre del producto que desea comprar, disponemos de: creatina, corebag, gymball, pressbanca y proteina"
-  );
+let addCarrito = (e) => {
+  console.log("Producto agregado al carrito", e.target);
 };
 
-do {
-  if (pregunta_lista == "si") {
-    console.log("Bienvenido al sistema");
-    compraProductoPrompt();
-    break;
-  }
-  if (pregunta_lista == "no") {
-    console.log("Sistema finalizado");
-    break;
-  }
-  if (pregunta_lista != "si" || "no") {
-    console.log("Datos incorrectos, responda con si o no");
-    pregunta_lista = prompt(
-      "¿quiere ver la lista de precios? responda con si o no"
-    );
-  }
-} while (pregunta_lista != "error");
-
-//Consulta de stock
-function buscarProducto(Stock_fitness) {
-  return Stock_fitness.producto == compraProducto;
+for (let boton of carrito) {
+  boton.addEventListener("click", addCarrito);
 }
 
-let resultadoProducto = listaProductos.find(buscarProducto);
-
-let cantidades;
-
-do {
-  if (resultadoProducto != undefined) {
-    console.log("Su producto es:", resultadoProducto);
-    if (resultadoProducto.get_stock()) {
-      cantidades = prompt("¿cuantas unidades va querer?");
-      if (resultadoProducto.renovar_stock(cantidades)) {
-        console.log("Gracias por su compra - fitnessar ©");
-      }
-    }
-    break;
-  }
-  if (resultadoProducto == undefined) {
-    console.log("Producto inexistente:", compraProducto);
-    break;
-  }
-} while (compraProducto != "error");
+//LOCALSTORAGE Y JSON CARRITO
+let aggCarrito = (Nombre, precio) => {
+  let changuito = JSON.parse(localStorage.getItem("cart")) || [];
+  changuito.push({ Nombre, precio });
+  localStorage.setItem("cart", JSON.stringify(changuito));
+};
